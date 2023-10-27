6a35429e7f56121552d12955b9dd66cae27ab018
dnlwlnc <dnlwlnc@users.noreply.github.com>
2023-10-27 16:13:53 +0200

    Changes to be committed: 	modified:   Ch01/01_06/begin/collections.py

--- Programming Foundations/Ch01/01_06/begin/collections.py
+++ Programming Foundations/Ch01/01_06/begin/collections.py
@@ -1,14 +1,38 @@
-# Nearest stars to Earth
-star1 = 'Sol'
-star2 = 'Alpha Centauri'
-star3 = 'Barnard'
-star4 = 'Wolf 359'
+# # Nearest stars to Earth
+# star1 = 'Sol'
+# star2 = 'Alpha Centauri'
+# star3 = 'Barnard'
+# star4 = 'Wolf 359'
 
-# Highest peak on each tectonic plate
-African = 'Kilimanjaro'
-Antarctic = 'Vinson'
-Australian = 'Puncak Jaya'
-Eurasian = 'Everest'
-North_American = 'Denali'
-Pacific = 'Mauna Kea'
-South_American = 'Aconcagua'
+# # Highest peak on each tectonic plate
+# African = 'Kilimanjaro'
+# Antarctic = 'Vinson'
+# Australian = 'Puncak Jaya'
+# Eurasian = 'Everest'
+# North_American = 'Denali'
+# Pacific = 'Mauna Kea'
+# South_American = 'Aconcagua'
+
+
+# Solution
+
+stars = [
+    'Sol',
+    'Alpha Centauri',
+    'Barnard',
+    'Wolf 359',
+]
+
+print(stars[3])
+
+peaks = {
+    'Africa': 'Kilimanjaro',
+    'Antarctica': 'Vinson',
+    'Australia': 'Puncak Jaya',
+    'Eurasia': 'Everest',
+    'North_America': 'Denali',
+    'Pacific': 'Mauna Kea',
+    'South_America': 'Aconcagua',
+}
+
+print(peaks['Pacific'])
