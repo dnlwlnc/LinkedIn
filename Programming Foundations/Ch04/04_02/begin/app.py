98a7e9a3873bd9fea4dfb1d079c146d975b7bb19
dnlwlnc <dnlwlnc@users.noreply.github.com>
2023-11-20 11:38:56 +0100

    Changes to be committed: 	modified:   Ch04/04_02/begin/app.py 	modified:   Ch04/04_05/begin/app.py

--- Programming Foundations/Ch04/04_02/begin/app.py
+++ Programming Foundations/Ch04/04_02/begin/app.py
@@ -1,3 +1,8 @@
-first_name = 'malala'
-last_name = 'yousafzai'
-note = 'award: Nobel Peace Prize'
+first_name = "malala"
+last_name = "yousafzai"
+note = "award: Nobel Peace Prize"
+
+first_name_cap = first_name.capitalize()
+last_name_cap = last_name.capitalize()
+print(first_name_cap)
+print(last_name_cap)
--- Programming Foundations/Ch04/04_05/begin/app.py
+++ Programming Foundations/Ch04/04_05/begin/app.py
@@ -1,2 +1,5 @@
-miles = input('Enter a distance in miles: ')
+miles = input("Enter a distance in miles: ")
 # kilometers_value = miles_value * 1.609344
+kilometer_value = float(miles) * 1.609344
+
+print("Distance in kilometers: " + str(kilometer_value) + " km.")
