e7d2c46fe27bad8a91991bbfc182fa6ccd01a4cd
dnlwlnc <dnlwlnc@users.noreply.github.com>
2023-11-20 13:21:25 +0100

    Changes to be committed: 	modified:   plant.py

--- Programming Foundations/Ch07/07_07/begin/plant.py
+++ Programming Foundations/Ch07/07_07/begin/plant.py
@@ -1,12 +1,12 @@
 def plant_recommendation(care):
-    if care = 'low':
-        print('aloe')
-    elif care == 'medium':
-        print('pothos')
-    elif care == 'medium':
-        print('orchid')
+    if care == "low":
+        print("aloe")
+    elif care == "medium":
+        print("pothos")
+    elif care == "high":
+        print("orchid")
 
-plant_rec('low')
-plant_recommendation('medium')
-plant_recommendation('high')
 
+plant_recommendation("low")
+plant_recommendation("medium")
+plant_recommendation("high")
